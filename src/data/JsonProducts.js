463bda4eb2fa3f4ce16122f9ff6279b2c99e202d
Juanes200122 <juanes22owen@gmail.com>
2024-10-02 16:18:19 -0500

    no message

--- src/data/JsonProducts.js
+++ src/data/JsonProducts.js
@@ -1,67 +1,132 @@
 export const JsonProducts = [
     {
-        clase: 'div1',
-        claseImagen: 'lapv1',
-        nombreProducto: 'Syltherine',
-        descripcion: 'Stylish cafe chair',
-        precio: '500.000',
-        descuento: '870.000'
+        detailsProducto: {
+            image: 'https://www.knoll.com/nkdc/images/inline/knoll-shop-and-browse_new.jpg',
+            nombreProducto: 'Syltherine',
+            descripcion: 'Stylish cafe chair',
+            precio: '500.000',
+            new_product: 'NEW'
+        },
+        descuentoProducto: {
+            aplica: 'SI',
+            cantidad_descuento: '-5%',
+            original_precio: '550.000'
+        },
+        detalles: 'true',
+        detalles_number: '1'
     },
     {
-        clase: 'div2',
-        claseImagen: 'lapv2',
-        nombreProducto: 'Massella Melanina',
-        descripcion: 'Stylish cafe chair',
-        precio: '2.500.000',
-        descuento: '3.870.000'
+        detailsProducto: {
+            image: 'https://decohunter.com/wp-content/uploads/2020/05/estilos-escritorios-modernos-industrial.jpg',
+            nombreProducto: 'Massella Melanina',
+            descripcion: 'Stylish cafe chair',
+            precio: '2.500.000',
+            new_product: ''
+        },
+        descuentoProducto: {
+            aplica: 'NO',
+            cantidad_descuento: '-20%',
+            original_precio: '3.870.000'
+        },
+        detalles: 'false',
+        detalles_number: '1'
     },
     {
-        clase: 'div3',
-        claseImagen: 'lapv3',
-        nombreProducto: 'slapters 4PT',
-        descripcion: 'Stylish cafe chair',
-        precio: '500.000',
-        descuento: '870.000'
+        detailsProducto: {
+            image: 'https://madesa.mx/cdn/shop/articles/Foto_Capa.jpg?v=1716894736&width=1080',
+            nombreProducto: 'slapters 4PT',
+            descripcion: 'Stylish cafe chair',
+            precio: '500.000',
+            new_product: 'NEW'
+        },
+        descuentoProducto: {
+            aplica: 'NO',
+            cantidad_descuento: '-20%',
+            original_precio: '870.000'
+        },
+        detalles: 'true',
+        detalles_number: '0'
     },
     {
-        clase: 'div4',
-        claseImagen: 'lapv4',
-        nombreProducto: 'Muebles Bikars',
-        descripcion: 'Luxury fix ',
-        precio: '2.500.000',
-        descuento: '3.870.000'
+        detailsProducto: {
+            image: 'https://i0.wp.com/bika.com.co/wp-content/uploads/2023/12/ALCOBA-LOREN-2-1.png?fit=800%2C800&ssl=1',
+            nombreProducto: 'Muebles Bikars',
+            descripcion: 'Luxury fix',
+            precio: '2.500.000',
+            new_product: 'NEW'
+        },
+        descuentoProducto: {
+            aplica: 'NO',
+            cantidad_descuento: '',
+            original_precio: ''
+        },
+        detalles: 'true',
+        detalles_number: '0'
     },
     {
-        clase: 'div5',
-        claseImagen: 'lapv5',
-        nombreProducto: 'Muebles Bikars',
-        descripcion: 'Stylish cafe chair',
-        precio: '500.000',
-        descuento: '870.000'
+        detailsProducto: {
+            image: 'https://lasilleria.cl/wp-content/uploads/2024/01/velador-nordico-minimal-mesa-lateral-de-disen%CC%83o-dos-cajones_.jpg',
+            nombreProducto: 'Muebles Bikars',
+            descripcion: 'Stylish cafe chair',
+            precio: '500.000',
+            new_product: ''
+        },
+        descuentoProducto: {
+            aplica: 'NO',
+            cantidad_descuento: '-20%',
+            original_precio: '870.000'
+        },
+        detalles: 'false',
+        detalles_number: '1'
     },
     {
-        clase: 'div6',
-        claseImagen: 'lapv6',
-        nombreProducto: 'Lampara Colgante',
-        descripcion: 'Stylish cafe chair',
-        precio: '2.500.000',
-        descuento: '3.870.000'
-    }
-    ,
-    {
-        clase: 'div7',
-        claseImagen: 'lapv7',
-        nombreProducto: 'Platon espejo',
-        descripcion: 'Stylish cafe chair',
-        precio: '500.000',
-        descuento: '870.000'
+        detailsProducto: {
+            image: 'https://http2.mlstatic.com/D_NQ_NP_706698-MCO73885369856_012024-O.webp',
+            nombreProducto: 'Lampara Colgante',
+            descripcion: 'Stylish cafe chair',
+            precio: '2.500.000',
+            new_product: 'NEW'
+        },
+        descuentoProducto: {
+            aplica: 'NO',
+            cantidad_descuento: '',
+            original_precio: ''
+        },
+        detalles: 'true',
+        detalles_number: '0'
     },
     {
-        clase: 'div8',
-        claseImagen: 'lapv8',
-        nombreProducto: 'Campanas de cocina',
-        descripcion: 'Minimalist',
-        precio: '2.500.000',
-        descuento: '3.870.000'
+        detailsProducto: {
+            image: 'https://m.media-amazon.com/images/I/6185T7aUOmL._AC_SL1500_.jpg',
+            nombreProducto: 'Platon espejo',
+            descripcion: 'Stylish cafe chair',
+            precio: '500.000',
+            new_product: 'NEW'
+        },
+        descuentoProducto: {
+            aplica: '1',
+            cantidad_descuento: '-10%',
+            original_precio: '3.870.000'
+        },
+        detalles: 'true',
+        detalles_number: '1'
+    },
+    {
+        detailsProducto: {   
+            image: 'https://i.pinimg.com/736x/37/37/bd/3737bd8eb5ef302560d4de7ab0683908.jpg',
+            nombreProducto: 'Campanas de cocina',
+            descripcion: 'Minimalist',
+            precio: '2.500.000',
+            new_product: ''
+        },
+        descuentoProducto: {
+            aplica: '1',
+            cantidad_descuento: '-10%',
+            original_precio: '3.870.000'
+        },
+        detalles: 'false',
+        detalles_number: '1'
     }
 ];
+
+
